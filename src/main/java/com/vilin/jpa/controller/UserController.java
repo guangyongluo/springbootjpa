0877a0b78104cf8cd8757a0b99ae9eb27e3a9874
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2020-03-06 00:09:02 +0800

    springbootjpa(com.vilin.jpa.repository.UserPagingAndSortingRepository.java;com.vilin.controller.UserController.java) spring data jpa method name create sql.

--- src/main/java/com/vilin/jpa/controller/UserController.java
+++ src/main/java/com/vilin/jpa/controller/UserController.java
@@ -4,14 +4,14 @@ import com.vilin.jpa.entity.User;
 import com.vilin.jpa.repository.UserPagingAndSortingRepository;
 import com.vilin.jpa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/demo")
@@ -47,11 +47,11 @@ public class UserController {
         userRepository.deleteById(id);
     }
 
-    @GetMapping(path = "page")
+    @GetMapping(path = "/page")
     @ResponseBody
     public Page<User> getAllUserByPage(){
         Sort.Order order = Sort.Order.asc("name");
-        Page<User> page =  userPagingAndSortingRepository.findAll(PageRequest.of(2, 2, Sort.by(order)));
+        Page<User> page =  userPagingAndSortingRepository.findAll(PageRequest.of(0, 2, Sort.by(order)));
         System.out.println("总页数：" + page.getTotalPages());
         System.out.println("总条数：" +page.getTotalElements());
         return page;
@@ -62,5 +62,22 @@ public class UserController {
     public Iterable<User> getAllUserWithSort(){
         Sort.Order order = Sort.Order.asc("name");
         return userPagingAndSortingRepository.findAll(Sort.by(order));
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public List<User> getUsersByPage(String email){
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<User> page = userPagingAndSortingRepository.findAllByEmailContaining(email, pageable);
+        System.out.println(page.getTotalElements());
+        return page.getContent();
+    }
+
+    @GetMapping("/slice")
+    @ResponseBody
+    public List<User> getUsersBySlice(String email){
+        Pageable pageable = PageRequest.of(0, 2);
+        Slice<User> slice = userPagingAndSortingRepository.findByEmailContaining(email, pageable);
+        return slice.getContent();
     }
 }
--- src/main/java/com/vilin/jpa/repository/UserPagingAndSortingRepository.java
+++ src/main/java/com/vilin/jpa/repository/UserPagingAndSortingRepository.java
@@ -1,8 +1,14 @@
 package com.vilin.jpa.repository;
 
 import com.vilin.jpa.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserPagingAndSortingRepository extends PagingAndSortingRepository<User, Long> {
 
+    Page<User> findAllByEmailContaining(String email, Pageable pageable);
+
+    Slice<User> findByEmailContaining(String email, Pageable pageable);
 }
